06aa1a9b76411fa3b9609ef963e097290084eb7f
Luka Martin <ldm3334@gmail.com>
2024-06-13 15:08:49 +1000

    Update skills section text and icons

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,25 +1,39 @@
 import React from "react";
 import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
 import { RiJavascriptFill } from "react-icons/ri";
+import { BiLogoTypescript } from "react-icons/bi";
+import { SiNextdotjs } from "react-icons/si";
 
 export default function Skills() {
 
     return (
         <div data-aos="zoom-in" className="bg-[#121212] border-[1px] border-[#252525] rounded-xl text-white p-6 md:p-8 mx-4 xl:mx-0 md:mx-[7.5rem] lg:mx-8 lg:w-[42%] lg:my-40 lg:max-h-[23.55rem]">
             <h2 className="text-xl md:text-2xl pb-8">Skills</h2>
-            <p className="mb-2">My skills currently include <span className="font-[600]">HTML</span>, <span className="font-[600]">CSS</span>, <span className="font-[600]">JavaScript</span> and <span className="font-[600]">React</span>.</p>
+            <p className="mb-2">My skills currently include <span className="font-[600]">HTML</span>, <span className="font-[600]">CSS</span>, <span className="font-[600]">JavaScript</span>, <span className="font-[600]">TypeScript</span>, <span className="font-[600]">React</span> and <span className="font-[600]">NextJs</span> .</p>
             <p> I am now progressing to backend development, to further expand my skills.</p>
             <div className="hidden md:flex justify-between pt-8 lg:pt-20">
                 <FaHtml5 size={50} className="text-[#F06529]" />
                 <FaCss3Alt size={50} className="text-[#2965f1]" />
                 <RiJavascriptFill size={50} className="text-[#f7df1e]" />
+                <BiLogoTypescript size={50} className="text-[#3178C6]" />
                 <FaReact size={50} className="text-[#61DBFB]" />
+                <SiNextdotjs size={50} className="text-black" />
             </div>
-            <div className="md:hidden flex justify-between pt-8 lg:pt-20">
+            <div className="hidden xs:flex md:hidden justify-between pt-8 lg:pt-20">
                 <FaHtml5 size={42} className="text-[#F06529]" />
                 <FaCss3Alt size={42} className="text-[#2965f1]" />
                 <RiJavascriptFill size={42} className="text-[#f7df1e]" />
+                <BiLogoTypescript size={42} className="text-[#3178C6]" />
                 <FaReact size={42} className="text-[#61DBFB]" />
+                <SiNextdotjs size={42} className="text-black" />
+            </div>
+            <div className="flex xs:hidden justify-between pt-8 lg:pt-20">
+                <FaHtml5 size={35} className="text-[#F06529]" />
+                <FaCss3Alt size={35} className="text-[#2965f1]" />
+                <RiJavascriptFill size={35} className="text-[#f7df1e]" />
+                <BiLogoTypescript size={35} className="text-[#3178C6]" />
+                <FaReact size={35} className="text-[#61DBFB]" />
+                <SiNextdotjs size={35} className="text-black" />
             </div>
         </div>
     )

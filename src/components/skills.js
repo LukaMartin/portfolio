d1538f2c2035f338d40c40a13f4fd0835a8c00b1
Luka Martin <ldm3334@gmail.com>
2024-09-25 08:53:56 +1000

    Update skills and about me text

--- src/components/skills.js
+++ src/components/skills.js
@@ -9,8 +9,8 @@ export default function Skills() {
     return (
         <div data-aos="zoom-in" className="bg-[#121212] border-[1px] border-[#252525] rounded-xl text-white p-6 md:p-8 mx-4 xl:mx-0 md:mx-[7.5rem] lg:mx-8 lg:w-[42%] lg:my-40 lg:max-h-[23.55rem]">
             <h2 className="text-xl md:text-2xl pb-8">Skills</h2>
-            <p className="mb-2">My skills currently include <span className="font-[600]">HTML</span>, <span className="font-[600]">CSS</span>, <span className="font-[600]">JavaScript</span>, <span className="font-[600]">TypeScript</span>, <span className="font-[600]">React</span> and <span className="font-[600]">NextJs</span> .</p>
-            <p> I am now progressing to backend development, to further expand my skills.</p>
+            <p className="mb-2">My skills currently include HTML, CSS, JavaScript, TypeScript, React and NextJs.</p>
+            <p> I also have backend exp in NodeJs, NestJs and PostgreSQL.</p>
             <div className="hidden md:flex justify-between pt-8 lg:pt-20">
                 <FaHtml5 size={50} className="text-[#F06529]" />
                 <FaCss3Alt size={50} className="text-[#2965f1]" />
